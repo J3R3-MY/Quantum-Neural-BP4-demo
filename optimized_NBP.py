62f67de58e5d0e2b88fff464b6df0d297711dc16
Jeremi <urnmd@student.kit.edu>
2025-09-05 17:40:21 +0200

    Prepare GB sims

--- optimized_NBP.py
+++ optimized_NBP.py
@@ -1113,52 +1113,40 @@ training_configs = {
 }
 
 base = init_and_train(
-    n=128, k=2, m=384, 
-    # n=200, k=2, m=600, 
-    # n=72, k=2, m=216, 
-    # n=32, k=2, m=96, 
     #GB
-    # n=254, k=28, m=254, 
+    n=254, k=28, m=254, 
     # n=126, k=28, m=126, 
     # n=48, k=6, m=2000, 
     # n=46, k=2, m=800, 
-    n_iterations=18, 
+    n_iterations=6, 
     error_weights=(4,7),
-    codeType='toric',
+    codeType='GB',
     params=training_configs['paper'],
     name="baseline-noopt"
 )
 
 one = init_and_train(
-    n=128, k=2, m=384, 
-    # n=200, k=2, m=600, 
-    # n=72, k=2, m=216, 
-    # n=32, k=2, m=96, 
     #GB
-    # n=254, k=28, m=254, 
+    n=254, k=28, m=254, 
     # n=126, k=28, m=126, 
     # n=48, k=6, m=2000, 
     # n=46, k=2, m=800, 
-    n_iterations=18, 
+    n_iterations=6, 
     error_weights=(5,6),
-    codeType='toric',
+    codeType='GB',
     params=training_configs['low_complexity_fast'],
     name="hamming-one"
 )
 
 two = init_and_train(
-    n=128, k=2, m=384, 
-    # n=200, k=2, m=600, 
-    # n=72, k=2, m=216, 
-    # n=32, k=2, m=96, 
     #GB
-    # n=254, k=28, m=254, 
+    n=254, k=28, m=254, 
     # n=126, k=28, m=126, 
     # n=48, k=6, m=2000, 
     # n=46, k=2, m=800, 
-    n_iterations=18, 
+    n_iterations=6, 
     error_weights=(6,7),
-    codeType='toric',
+    codeType='GB',
     params=training_configs['low_complexity_fast'],
     name="hamming-two"
 )
@@ -1167,10 +1155,11 @@ print("baseline performance, ep = 0.4")
 cpp_base = get_binary(type = 'base', ep = 'ep04', decoder = base )
 subprocess.call([cpp_base])
 
-print("unpruned ensemble, ep = 0.4")
+print("unpruned ensemble guess, ep = 0.4")
 cpp_guess = get_binary(type = 'guess', ep = 'ep04', decoder = base )
 subprocess.call([cpp_guess])
 
+print("unpruned ensemble list, ep = 0.4")
 cpp_list = get_binary(type = 'list', ep = 'ep04', decoder = base )
 subprocess.call([cpp_list])
 
@@ -1184,13 +1173,12 @@ train(one, training_configs['low_complexity_fast'])
 two.prune_weights(0.33)
 train(two, training_configs['low_complexity_fast'])
 
-
-print("Pruned baseline")
-subprocess.call([cpp_base])
-
 print("Now calling binaries for evaluation...")
 print("Guess error rate, pruned, ep = 0.4")
 subprocess.call([cpp_guess])
+
+print("List error rate, pruned, ep = 0.4")
+subprocess.call([cpp_list])
 
 print("Training and pruning completed.\n")
 
